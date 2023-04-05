c1512a1278db855451a27ac9d13bc8e2b908b2b8
Akarsh Srivastava <Akarsh0801@users..github.com>
2023-04-05 19:43:27 +0530

    eight

--- src/About.js
+++ src/About.js
@@ -16,7 +16,7 @@ export default function About(){
 
 
 {/* Left side */}
-    <div id='rightContent' className="flex h-full w-full mb-10">
+    <div id='rightContent' className="flex h-full w-full mb-10 md:mb-0 lg:mb-0 xl:mb-0 2xl:mb-0 3xl:mb-0">
        <div className=" h-screen w-full p-6 md:p-14 lg:p-20 xl:p-28 2xl:p-28  bg-gray-100  flex flex-row  justify-start items-center" >
         <div className="flex flex-col xl:flex-row-reverse gap-y-3 justify-between  lg:gap-4 xl:gap-4 2xl:gap-4 items-start  md:w-full lg:w-full xl:w-full 2xl:w-4/5 3xl:w-4/5">
         <div className="rounded-full h-72 w-72 md:h-72 md:w-72 lg:h-72 lg:w-72 xl:h-80 xl:w-80 2xl:h-96 2xl:w-96  shadow-lg shadow-blue-400 flex justify-center items-center ">
@@ -33,7 +33,7 @@ export default function About(){
             </div></Link>
             <div className="flex flex-row gap-x-2">
             <button className="h-auto rounded-full w-fit border-2 hover:shadow-md hover:shadow-black border-black hover:border-blue-700 p-2  font-fontFjalla hover:scale-105 hover:bg-blue-700 hover:text-white text-black transition-all  duration-500 hover:ease-in-out">Download CV</button>
-            <button className="h-auto rounded-full w-fit border-2 hover:shadow-md hover:shadow-black border-black hover:border-blue-700 p-2  font-fontFjalla hover:scale-105 hover:bg-blue-700 hover:text-white text-black transition-all  duration-500 hover:ease-in-out" onClick={Certificates}>Certificates</button>
+            <button className="h-auto rounded-full visible md:hidden lg:hidden xl:hidden 2xl:hidden 3xl:hidden w-fit border-2 hover:shadow-md hover:shadow-black border-black hover:border-blue-700 p-2  font-fontFjalla hover:scale-105 hover:bg-blue-700 hover:text-white text-black transition-all  duration-500 hover:ease-in-out" onClick={Certificates}>Certificates</button>
             </div>
         </div>
         </div>
